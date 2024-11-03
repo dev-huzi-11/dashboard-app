a8c2a6929320513f4162032ff081b9e87b1f0a9e
Huzaifa <162676983+dev-huzi-11@users.noreply.github.com>
2024-11-03 11:39:39 +0500

    dashboard

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <p>This is customer page</p>
+    </div>
+  )
+}
+
+export default page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <p>This is dashboard</p>
+    </div>
+  )
+}
+
+export default page
